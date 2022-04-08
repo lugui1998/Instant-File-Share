53b32d5cfa7f52223c293117803777ede7b452a7
lugui <lugui1998@hotmail.com>
2022-04-08 17:17:38 -0300

    Update setup script

--- setup.bat
+++ setup.bat
@@ -1,13 +1,7 @@
-SET node_path=%~dp0node.exe
+where node > tmpFile
+SET /p node_path= < tmpFile
+
 SET mypath=%~dp0
 set copyPath=%mypath%command\copy.js
 
-::set command=REG ADD "HKEY_CLASSES_ROOT\*\shell\Copy Share URL\command" /f /ve /d "%node_path%" "%copyPath%" "%%1"
-set command=REG ADD "HKEY_CLASSES_ROOT\*\shell\Copy Share URL\command" /f /ve /d "\"%node_path%\" \"%copyPath%\" \"%%1\""
-
-
-%command%
-
-
-
-
+reg add "HKEY_CLASSES_ROOT\*\shell\Copy Share URL\command" /f /ve /d "\"%node_path%\" \"%copyPath%\" \"%%1""
