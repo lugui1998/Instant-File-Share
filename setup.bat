f0f7ef35a520acd002281b51bee27a325dd5a64f
lugui <lugui1998@hotmail.com>
2022-04-10 10:16:45 -0300

    Add startup script

--- setup.bat
+++ setup.bat
@@ -6,3 +6,6 @@ SET mypath=%~dp0
 set copyPath=%mypath%command\copy.js
 
 reg add "HKEY_CLASSES_ROOT\*\shell\Copy Share URL\command" /f /ve /d "\"%node_path%\" \"%copyPath%\" \"%%1""
+
+echo cd /d "%mypath%" > "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ifs.bat"
+echo npx pm2 start ./dist/server.js --name instant-file-share --interpreter=node --watch >> "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ifs.bat"
